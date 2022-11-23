f91a1b268f5c29c247a24d52c4cc3db8e0f4544b
aslambro1882 <0909maruf@gmail.com>
2022-11-23 23:42:43 +0600

    message

--- app/models.py
+++ app/models.py
@@ -19,21 +19,17 @@ class Formfillup(models.Model):
 
     student_id = models.CharField(max_length=200, verbose_name='student_id',)
 
-    semester = models.CharField(
-        max_length=50, verbose_name='semester', default='')
+    semester = models.CharField(max_length=50, verbose_name='semester', default='')
 
     session = models.CharField(max_length=50, verbose_name='session',)
 
     hall = models.CharField(max_length=200, verbose_name='hall', default='')
 
-    department = models.CharField(
-        max_length=200, verbose_name='department', default='')
+    department = models.CharField(max_length=200, verbose_name='department', default='')
 
-    exam_year = models.CharField(
-        max_length=50, verbose_name='exam_year', default='')
+    exam_year = models.CharField(max_length=50, verbose_name='exam_year', default='')
 
-    exam_starting_date = models.DateField(
-        verbose_name='exam_starting_date', default=None, blank=True, null=True)
+    exam_starting_date = models.DateField(verbose_name='exam_starting_date', default=None, blank=True, null=True)
 
     # ----------COURSE CODES-------
     code1 = models.CharField(max_length=50, verbose_name='code1', default='')
@@ -46,47 +42,34 @@ class Formfillup(models.Model):
     code8 = models.CharField(max_length=50, verbose_name='code8', default='')
     code9 = models.CharField(max_length=50, verbose_name='code9', default='')
 
-    last_semester = models.CharField(
-        max_length=50, verbose_name='last_semester', default='')
+    last_semester = models.CharField(max_length=50, verbose_name='last_semester', default='')
 
-    passing_year = models.CharField(
-        max_length=50, verbose_name='passing_year', default='')
+    passing_year = models.CharField(max_length=50, verbose_name='passing_year', default='')
 
-    result = models.CharField(
-        max_length=50, verbose_name='result', default='')
+    result = models.CharField(max_length=50, verbose_name='result', default='')
 
     name = models.CharField(max_length=200, verbose_name='Name', )
 
-    father = models.CharField(
-        max_length=200, verbose_name='father', default='',)
+    father = models.CharField(max_length=200, verbose_name='father', default='',)
 
-    mother = models.CharField(
-        max_length=200, verbose_name='mother', default='')
+    mother = models.CharField(max_length=200, verbose_name='mother', default='')
 
-    guardian = models.CharField(
-        max_length=200, verbose_name='guardian', default='')
+    guardian = models.CharField(max_length=200, verbose_name='guardian', default='')
 
     phone = models.CharField(max_length=50, verbose_name='phone', default=' ')
 
-    birth_date = models.DateField(
-        verbose_name='birth_date', default=None, blank=True, null=True)
+    birth_date = models.DateField(verbose_name='birth_date', default=None, blank=True, null=True)
 
-    nationality = models.CharField(
-        max_length=200, verbose_name='nationality', default='')
+    nationality = models.CharField(max_length=200, verbose_name='nationality', default='')
 
-    religion = models.CharField(
-        max_length=200, verbose_name='religion', default='')
+    religion = models.CharField(max_length=200, verbose_name='religion', default='')
 
-    village = models.CharField(
-        max_length=200, verbose_name='village', default='')
+    village = models.CharField(max_length=200, verbose_name='village', default='')
 
-    post_office = models.CharField(
-        max_length=200, verbose_name='Post_office', default='',)
+    post_office = models.CharField(max_length=200, verbose_name='Post_office', default='',)
 
-    upazila = models.CharField(
-        max_length=200, verbose_name='upazila', default='')
+    upazila = models.CharField(max_length=200, verbose_name='upazila', default='')
 
-    district = models.CharField(
-        max_length=200, verbose_name='district', default='')
+    district = models.CharField(max_length=200, verbose_name='district', default='')
 
     # TODO: ekhan theke aro add kor field ja ja lagbe..
